8658375cb2096e6b4f8696c9cac88207ea9f9124
Parth Shah <helloparthshah@gmail.com>
2022-04-05 18:27:49 -0700

    started lab 2

---
+++ Lab 2/SPI_SLAVE/spi_demo/Release/main.d
@@ -0,0 +1,37 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/string.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/linkage.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_types.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_memmap.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_common_reg.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_ints.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/spi.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_map.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_patch.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/utils.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/prcm.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/uart.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/interrupt.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/example/common/uart_if.h
+main.obj: ../pinmux.h
+
+../main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/string.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/linkage.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_types.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_memmap.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_common_reg.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_ints.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/spi.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_map.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_patch.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/utils.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/prcm.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/uart.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/interrupt.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/example/common/uart_if.h: 
+../pinmux.h: 
